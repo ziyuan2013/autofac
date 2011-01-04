8bcfd04822f90d0963da432d4b12995501bd0cdc
s_hebert <s_hebert@localhost>
2011-01-04 01:24:13 -0600

    added typeof(T) metadata registration in the AutofacContrib.Attributed MetadataModule along with unit tests

---
+++ src/Test/Autofac.Tests.Integration.Wcf/AutofacWebServiceHostFactoryFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Autofac.Integration.Wcf;
+using Autofac.Builder;
+using Autofac.Core;
+
+namespace Autofac.Tests.Integration.Wcf
+{
+    [TestFixture]
+    public class AutofacWebServiceHostFactoryFixture
+    {
+        static readonly Uri[] DummyEndpoints = new[] { new Uri("http://localhost") };
+
+        [Test]
+        public void HostsKeyedServices()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<object>().Named<object>("service");
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacWebServiceHostFactory();
+                var host = factory.CreateServiceHost("service", DummyEndpoints);
+                Assert.IsNotNull(host);
+            });
+        }
+
+        [Test]
+        public void HostsTypedServices()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<object>();
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacWebServiceHostFactory();
+                var host = factory.CreateServiceHost(typeof(object).FullName, DummyEndpoints);
+                Assert.IsNotNull(host);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DetectsUnknownImplementationTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<ITestService>(c => new TestService()).Named<object>("service");
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacWebServiceHostFactory();
+                factory.CreateServiceHost("service", DummyEndpoints);
+            });
+        }
+
+        static void TestWithHostedContainer(IContainer container, Action test)
+        {
+            AutofacHostFactory.Container = container;
+            try
+            {
+                test();
+            }
+            finally
+            {
+                AutofacHostFactory.Container = null;
+            }
+        }
+    }
+}

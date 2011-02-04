477fe0e0e3fbce264808dd82849811b3314f91da
nicholas.blumhardt <nicholas.blumhardt@localhost>
2011-02-04 20:50:22 +1000

    Ensure disposed lifetime scopes can't be used.

---
+++ src/Test/Autofac.Tests.Integration.Wcf/AutofacServiceHostFactoryFixture.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NUnit.Framework;
+using Autofac.Integration.Wcf;
+
+namespace Autofac.Tests.Integration.Wcf
+{
+    [TestFixture]
+    public class AutofacServiceHostFactoryFixture
+    {
+        static readonly Uri[] DummyEndpoints = new[] { new Uri("http://localhost") };
+
+        [Test]
+        public void HostsKeyedServices()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<object>().Named<object>("service");
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacServiceHostFactory();
+                var host = factory.CreateServiceHost("service", DummyEndpoints);
+                Assert.AreEqual(typeof(object), host.Description.ServiceType);
+                Assert.IsNotNull(host);
+            });
+        }
+
+        [Test]
+        public void HostsTypedServices()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<object>();
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacServiceHostFactory();
+                var host = factory.CreateServiceHost(typeof(object).FullName, DummyEndpoints);
+                Assert.IsNotNull(host);
+                Assert.AreEqual(typeof(object), host.Description.ServiceType);
+            });
+        }
+
+        [Test]
+        public void HostsTypedServicesAsServices()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register(c => "Test").As<object>();
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacServiceHostFactory();
+                var host = factory.CreateServiceHost(typeof(object).FullName, DummyEndpoints);
+                Assert.IsNotNull(host);
+                Assert.AreEqual(typeof(string), host.Description.ServiceType);
+            });
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DetectsUnknownImplementationTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<ITestService>(c => new TestService()).Named<object>("service");
+            TestWithHostedContainer(builder.Build(), () =>
+            {
+                var factory = new AutofacServiceHostFactory();
+                factory.CreateServiceHost("service", DummyEndpoints);
+            });
+        }
+
+        static void TestWithHostedContainer(IContainer container, Action test)
+        {
+            AutofacHostFactory.Container = container;
+            try
+            {
+                test();
+            }
+            finally
+            {
+                AutofacHostFactory.Container = null;
+            }
+        }
+    }
+}

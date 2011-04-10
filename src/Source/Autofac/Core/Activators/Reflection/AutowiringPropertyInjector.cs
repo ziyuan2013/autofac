420e268ec33a9a7bdab989c7779518b307c2a392
nicholas.blumhardt <nicholas.blumhardt@localhost>
2011-04-10 20:32:45 +1000

    Fixes issue 314 - undefined behaviour when selecting a constructor from many with the same number of resolvable parameters.

---
+++ src/Source/Autofac/Core/Activators/Reflection/AutowiringPropertyInjector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using Autofac.Util;
+
+namespace Autofac.Core.Activators.Reflection
+{
+    class AutowiringPropertyInjector
+    {
+        public void InjectProperties(IComponentContext context, object instance, bool overrideSetValues)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+            if (instance == null) throw new ArgumentNullException("instance");
+
+            Type instanceType = instance.GetType();
+
+            foreach (PropertyInfo property in instanceType.GetProperties(
+                BindingFlags.Public | BindingFlags.Instance | BindingFlags.SetProperty))
+            {
+                Type propertyType = property.PropertyType;
+
+                if (propertyType.IsValueType)
+                    continue;
+
+                if (property.GetIndexParameters().Length != 0)
+                    continue;
+
+                if (!context.IsRegistered(propertyType))
+                    continue;
+
+                var accessors = property.GetAccessors(false);
+                if (accessors.Length == 1 && accessors[0].ReturnType != typeof(void))
+                    continue;
+
+                if (!overrideSetValues &&
+                    accessors.Length == 2 &&
+                    (property.GetValue(instance, null) != null))
+                    continue;
+
+                object propertyValue = context.Resolve(propertyType);
+                property.SetValue(instance, propertyValue, null);
+            }
+        }
+    }
+}
---
+++ src/Test/Autofac.Tests.Integration.Mvc/RegistrationExtensionsFixture.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using Autofac.Builder;
+using Autofac.Integration.Mvc;
+using NUnit.Framework;
+
+namespace Autofac.Tests.Integration.Mvc
+{
+    [TestFixture]
+    public class RegistrationExtensionsFixture
+    {
+        [Test]
+        public void RegisterModelBinderProviderThrowsExceptionForNullBuilder()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(
+                () => Autofac.Integration.Mvc.RegistrationExtensions.RegisterModelBinderProvider(null));
+            Assert.That(exception.ParamName, Is.EqualTo("builder"));
+        }
+
+        [Test]
+        public void RegisterModelBindersThrowsExceptionForNullBuilder()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(
+                () => Autofac.Integration.Mvc.RegistrationExtensions.RegisterModelBinders(null, Assembly.GetExecutingAssembly()));
+            Assert.That(exception.ParamName, Is.EqualTo("builder"));
+        }
+
+        [Test]
+        public void RegisterModelBindersThrowsExceptionForNullAssemblies()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(
+                () => new ContainerBuilder().RegisterModelBinders(null));
+            Assert.That(exception.ParamName, Is.EqualTo("modelBinderAssemblies"));
+        }
+
+        [Test]
+        public void CacheInSessionThrowsExceptionForNullRegistration()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(
+                () => Autofac.Integration.Mvc.RegistrationExtensions.CacheInSession<object, SimpleActivatorData, SingleRegistrationStyle>(null));
+            Assert.That(exception.ParamName, Is.EqualTo("registration"));
+        }
+
+        [Test]
+        public void InvokesCustomActivating()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(GetType().Assembly)
+                .OnActivating(e => ((ModuleTestController)e.Instance).Dependency = new object());
+
+            var container = builder.Build();
+
+            var controller = container.Resolve<ModuleTestController>();
+            Assert.IsNotNull(controller.Dependency);
+        }
+
+        [Test]
+        public void InjectsInvoker()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(GetType().Assembly)
+                .InjectActionInvoker();
+            builder.RegisterType<TestActionInvoker>().As<IActionInvoker>();
+            var container = builder.Build();
+
+            var controller = container.Resolve<ModuleTestController>();
+            Assert.IsInstanceOf<TestActionInvoker>(controller.ActionInvoker);
+        }
+
+
+        [Test]
+        public void DoesNotRegisterControllerTypesThatDoNotEndWithControllerString()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(GetType().Assembly);
+
+            var container = builder.Build();
+
+            Assert.IsFalse(container.IsRegistered<IsAControllerNot>());
+        }
+
+        public class TestActionInvoker : IActionInvoker
+        {
+            public bool InvokeAction(ControllerContext controllerContext, string actionName)
+            {
+                return true;
+            }
+        }
+
+        public class ModuleTestController : Controller
+        {
+            public object Dependency;
+        }
+
+        public class IsAControllerNot : Controller
+        {
+        }
+    }
+}

b260aaa0e9bc78275eacb2c2569c610ca647dcaa
nicholas.blumhardt <nicholas.blumhardt@localhost>
2011-07-01 08:31:36 +1000

    Started work to get MEF integration running on SL4.

---
+++ src/Test/Autofac.Tests.Integration.Mvc/AutofacWebTypesModuleFixture.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Hosting;
+using Autofac.Integration.Mvc;
+using NUnit.Framework;
+
+namespace Autofac.Tests.Integration.Mvc
+{
+    [TestFixture]
+    public class AutofacWebTypesModuleFixture
+    {
+        [Test]
+        public void EnsureHttpContextRelatedTypesRegistered()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacWebTypesModule());
+            IContainer container = builder.Build();
+
+            Assert.That(container.IsRegistered<HttpContextBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpRequestBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpResponseBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpServerUtilityBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpSessionStateBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpApplicationStateBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpBrowserCapabilitiesBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpFileCollectionBase>(), Is.True);
+            Assert.That(container.IsRegistered<HttpCachePolicyBase>(), Is.True);
+            Assert.That(container.IsRegistered<VirtualPathProvider>(), Is.True);
+        }
+    }
+}

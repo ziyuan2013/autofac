5a38eb2be7d5ec12afc48b4a94221f5b4587d953
alex.meyergleaves <alex.meyergleaves@localhost>
2012-10-17 23:23:28 +1000

    Fixed complex generic and open generic unit tests that were ignored because Type.GUID is not available in PCL.

---
+++ Extras/Source/Autofac.Extras.CommonServiceLocator/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Autofac.Extras.CommonServiceLocator")]
+[assembly: AssemblyDescription("Autofac Adapter for the Microsoft CommonServiceLocator")]
+[assembly: ComVisible(false)]

08cf40c3f69dbd04157e424a79f3e9e84acb4c81
alex.meyergleaves <alex.meyergleaves@localhost>
2012-12-08 22:37:34 +1000

    Resolved issue 391: The same registration can now be used to register a MVC and Web API filter. Multiple filter types on a single registration are also supported.

---
+++ Core/Source/Autofac.Integration.Wcf/AutofacInstanceContext.cs
@@ -0,0 +1,134 @@
+﻿// This software is part of the Autofac IoC container
+// Copyright © 2011 Autofac Contributors
+// http://autofac.org
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using Autofac.Core;
+using Autofac.Core.Registration;
+
+namespace Autofac.Integration.Wcf
+{
+    /// <summary>
+    /// Manages instance lifecycle using an Autofac inner container.
+    /// </summary>
+    public class AutofacInstanceContext : IExtension<InstanceContext>, IDisposable, IComponentContext
+    {
+        readonly ILifetimeScope _lifetime;
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacInstanceContext"/> class.
+        /// </summary>
+        /// <param name="container">The outer container.</param>
+        public AutofacInstanceContext(ILifetimeScope container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+
+            _lifetime = container.BeginLifetimeScope();
+        }
+
+        ~AutofacInstanceContext()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Enables an extension object to find out when it has been aggregated.
+        /// Called when the extension is added to the
+        /// <see cref="P:System.ServiceModel.IExtensibleObject`1.Extensions"/> property.
+        /// </summary>
+        /// <param name="owner">The extensible object that aggregates this extension.</param>
+        public void Attach(InstanceContext owner)
+        {
+        }
+
+        /// <summary>
+        /// Enables an object to find out when it is no longer aggregated.
+        /// Called when an extension is removed from the
+        /// <see cref="P:System.ServiceModel.IExtensibleObject`1.Extensions"/> property.
+        /// </summary>
+        /// <param name="owner">The extensible object that aggregates this extension.</param>
+        public void Detach(InstanceContext owner)
+        {
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or
+        /// resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Handles disposal of managed and unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <see langword="true" /> to dispose of managed resources (during a manual execution
+        /// of <see cref="Autofac.Integration.Wcf.AutofacInstanceContext.Dispose()"/>); or
+        /// <see langword="false" /> if this is getting run as part of finalization where
+        /// managed resources may have already been cleaned up.
+        /// </param>
+        private void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    // Free managed resources
+                    _lifetime.Dispose();
+                }
+                this._disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Associates services with the components that provide them.
+        /// </summary>
+        public IComponentRegistry ComponentRegistry
+        {
+            get { return _lifetime.ComponentRegistry; }
+        }
+
+        /// <summary>
+        /// Resolve an instance of the provided registration within the context.
+        /// </summary>
+        /// <param name="registration">The registration.</param>
+        /// <param name="parameters">Parameters for the instance.</param>
+        /// <returns>
+        /// The component instance.
+        /// </returns>
+        /// <exception cref="ComponentNotRegisteredException"/>
+        /// <exception cref="Autofac.Core.DependencyResolutionException"/>
+        public object ResolveComponent(IComponentRegistration registration, IEnumerable<Parameter> parameters)
+        {
+            return _lifetime.ResolveComponent(registration, parameters);
+        }
+    }
+}
---
+++ Extras/Source/Autofac.Extras.Multitenant/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Autofac.Extras.Multitenant")]
+[assembly: AssemblyDescription("Autofac multitenancy support library.")]
+[assembly: ComVisible(false)]

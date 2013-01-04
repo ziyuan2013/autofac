46e60fa5695f4aaadeadccf6dda2f9313d61d530
alex.meyergleaves <alex.meyergleaves@localhost>
2013-01-04 22:24:37 +1000

    Version information for Beta 2 build.

---
+++ Extras/Source/Autofac.Extras.DynamicProxy2/InterceptAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
+using Autofac.Core;
+using Castle.DynamicProxy;
+
+namespace Autofac.Extras.DynamicProxy2
+{
+    [SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments")]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public sealed class InterceptAttribute : Attribute
+    {
+        public Service InterceptorService { get; private set; }
+
+        public InterceptAttribute(Service interceptorService)
+        {
+            if (interceptorService == null)
+                throw new ArgumentNullException("interceptorService");
+
+            this.InterceptorService = interceptorService;
+        }
+
+        [SecuritySafeCritical]
+        public InterceptAttribute(string interceptorServiceName)
+            : this(new KeyedService(interceptorServiceName, typeof(IInterceptor)))
+        {
+        }
+
+        public InterceptAttribute(Type interceptorServiceType)
+            : this(new TypedService(interceptorServiceType))
+        {
+        }
+    }
+}

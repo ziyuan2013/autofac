48f2fd66ae547ea298447c8773ca48dd5085de7c
travis illig <travis.illig@gmail.com>
2013-12-17 12:29:51 -0800

    Resolved Issue #269: Func<T, U, ...> auto-generated factories no longer allow execution/resolution if the function signature has duplicate input types. You can still register overrides or custom delegates if you need this functionality. Updated core Autofac version to 3.3.0 in preparation for publish.

---
+++ Examples/Remember.Persistence.NHibernate/NHibernateModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Cfg;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace Remember.Persistence.NHibernate
+{
+    public class NHibernateModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException("builder");
+
+            builder.RegisterGeneric(typeof(NHibernateRepository<>))
+                .As(typeof(IRepository<>))
+                .InstancePerHttpRequest();
+
+            builder.Register(c => new TransactionTracker())
+                .InstancePerHttpRequest();
+
+            builder.Register(c => c.Resolve<ISessionFactory>().OpenSession())
+                .InstancePerHttpRequest()
+                .OnActivated(e => e.Context.Resolve<TransactionTracker>().CurrentTransaction =
+                    e.Instance.BeginTransaction());
+
+            builder.Register(c => new Configuration().Configure().BuildSessionFactory())
+                .SingleInstance();
+        }
+    }
+}
---
+++ Extras/Source/Autofac.Extras.DomainServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Autofac.Extras.DomainServices")]
+[assembly: ComVisible(false)]
